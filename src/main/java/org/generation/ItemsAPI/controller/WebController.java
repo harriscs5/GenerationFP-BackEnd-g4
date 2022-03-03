bba01a9152e99926f73d4ecd4db215c42c173cb5
Nelly <$>
2022-03-03 18:42:19 -0500

    add new item form to nav bar

--- src/main/java/org/generation/ItemsAPI/controller/WebController.java
+++ src/main/java/org/generation/ItemsAPI/controller/WebController.java
@@ -1,4 +1,4 @@
-package com.dawti.itemsapi.controller;
+package org.generation.ItemsAPI.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
